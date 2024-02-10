99bc53bc05cd0cc49c369745c5327fe268b7951a
CreedTrue <34081464+CreedTrue@users.noreply.github.com>
2024-02-09 20:09:20 -0700

    Add project files.

---
+++ FirstNetApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FirstNetApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
